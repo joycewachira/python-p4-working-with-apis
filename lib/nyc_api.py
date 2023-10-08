fd138d4de70f5f088ba9f9eb3ac2cfa3ee53e21b
Joyce <joycewachira718@gmail.com>
2023-10-08 12:30:59 +0300

    Completed

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -5,11 +5,24 @@ import json
 class GetPrograms:
 
   def get_programs(self):
-    URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+    try:
+      URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+      response=requests.get(URL)
+      
+      return response.content
+    except Exception as e:
+      print("we got this error", str(e))
 
-    response = requests.get(URL)
-    return response.content
+  def program_school(self):
+    program_list=[]
+    programs=json.loads(self.get_programs())
+    for program in programs:
+      program_list.append(program["agency"])
+    return program_list
 
 
-programs = GetPrograms().get_programs()
-print(programs)
+intance =GetPrograms()
+# print(intance.get_programs())
+programs_school =intance.program_school()
+for school in set(programs_school):
+  print(school)
